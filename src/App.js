9158da51f5512976f3253ca51f2bcff265b516b7
Chris Santamaria <chris@santamaria.me>
2019-10-06 13:50:41 -0400

    Resized typography

--- src/App.js
+++ src/App.js
@@ -20,23 +20,25 @@ const IconLink = ({ title, link, children }) => (
 
 export default function App() {
   return (
-    <Container maxWidth="lg" className="container">
-      <Typography variant="h1" style={{ marginBottom: 10 }}>
+    <Container maxWidth="md" className="container">
+      <Typography variant="h2" style={{ marginBottom: 10 }}>
         Hi, I'm Chris Santamaria.
       </Typography>
-      <Typography variant="h3" style={{ marginBottom: 70 }}>
+      <Typography variant="h4" style={{ marginBottom: 40 }}>
         Student, fullstack web developer, and avid learner
       </Typography>
 
-      <Typography variant="h5" component="p" style={{ marginBottom: 20 }}>
-        I'm a Computer Science engineering student at the University of Virginia
-        with experience in numerous software engineering work environments.
-        Primarily skilled in full-stack web app development with Node.js and
+      <Typography variant="h6" style={{ marginBottom: 20 }}>
+        I'm a Computer Science student at the University of Virginia with
+        experience in numerous software engineering work environments. I'm
+        primarily skilled in full-stack web app development with Node.js and
         React but always looking for new areas to expand my skill set.
       </Typography>
-      <Typography variant="h5" component="p" style={{ marginBottom: 40 }}>
+      <Typography variant="h6" component="p" style={{ marginBottom: 40 }}>
         Feel free to reach out through any of the platforms below or view my
-        resume. I'd love to get in touch!
+        resume.
+        <br />
+        I'd love to get in touch!
       </Typography>
 
       <div style={{ display: 'flex', alignItems: 'center' }}>
