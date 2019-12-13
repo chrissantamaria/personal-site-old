e878b9dd32f03642fff7ce82c1e5cc6be9a17bfa
Chris Santamaria <chris@santamaria.me>
2019-12-12 23:54:48 -0500

    Add gatsby-plugin-material-ui

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,14 @@ module.exports = {
         ]
       }
     },
+    {
+      resolve: `gatsby-plugin-material-ui`,
+      options: {
+        stylesProvider: {
+          injectFirst: true
+        }
+      }
+    },
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
